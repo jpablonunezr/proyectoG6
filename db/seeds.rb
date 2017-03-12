ae2657ef5b218982a6358139a20f2e9f1b17cbb1
jpablonunezr <jpablo.nunezr@gmail.com>
2017-03-11 22:21:34 -0300

    css en vistas

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ User.destroy_all
 Level.destroy_all
 Subject.destroy_all
 
-User.create(email:'a@a', password:'123456')
-User.create(email:'b@b', password:'123456')
+User.create(email:'a@a', password:'123456', name:'alejandra', city:'Santiago')
+User.create(email:'b@b', password:'123456', name:'Pablo', city:'Santiago')
 
 
 Level.create(name:'Pre-Kínder')
