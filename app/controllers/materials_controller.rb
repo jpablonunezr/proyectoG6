ac74d36f90de32d40b1af21c26781fd6ba5935d2
Gonzalo <gonzalo.sanchez.d@gmail.com>
2017-03-17 15:54:07 -0300

    add blank question on material form

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -16,6 +16,7 @@ class MaterialsController < ApplicationController
   # GET /materials/new
   def new
     @material = Material.new
+    @material.questions.build()
   end
 
   # GET /materials/1/edit
