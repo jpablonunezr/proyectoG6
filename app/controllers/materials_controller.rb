3e3468201f6eb2006b40862294b2e001fa3dc1c5
jpablonunezr <jpablo.nunezr@gmail.com>
2017-03-17 19:45:23 -0300

    vista

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -34,7 +34,7 @@ class MaterialsController < ApplicationController
 
     respond_to do |format|
       if @material.save
-        format.html { redirect_to edit_material_path(@material), notice: 'Material was successfully created.' }
+        format.html { redirect_to root_path(@material), notice: 'Material was successfully created.' }
         format.json { render :show, status: :created, location: @material }
         format.js
       else
