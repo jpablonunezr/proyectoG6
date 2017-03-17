7117f972fc812150b58b536209e2f5105410eda0
Gonzalo <gonzalo.sanchez.d@gmail.com>
2017-03-17 16:03:20 -0300

    add id of questions to the strong params

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -77,6 +77,6 @@ class MaterialsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def material_params
-      params.require(:material).permit(:title, :description, :created_at, :updated_at, :updated_by, :public_level, :owner, :level_id, :subject_id, questions_attributes: [:content, :_destroy, alternatives_attributes: [:content, :_destroy] ])
+      params.require(:material).permit(:title, :description, :created_at, :updated_at, :updated_by, :public_level, :owner, :level_id, :subject_id, questions_attributes: [:id, :content, :_destroy, alternatives_attributes: [:id, :content, :_destroy] ])
     end
 end
