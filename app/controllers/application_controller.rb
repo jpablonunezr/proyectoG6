35b800a9412a182e5fd2a00a21aede1f3a9a41ce
Alejandra Diaz <ygthar@gmail.com>
2017-03-18 22:29:25 -0300

    diagram

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  private
-   	def filter_admin!
-		  authenticate_user!
-	  	redirect_to root_path, alert: "No tienes acceso" unless current_user.admin?
- 	end
 end
