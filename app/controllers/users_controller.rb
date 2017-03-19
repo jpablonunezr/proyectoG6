17eb4fc7a028699f3f3f25cf641c5936f979f4c5
Alejandra Diaz <ygthar@gmail.com>
2017-03-19 17:22:21 -0300

    buscador

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
   def index
   	if params[:search].present?
-      @users = User.where("lower(first_name) like ? OR lower(last_name) like ?", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
-    else
+      @users = User.where("lower(first_name) like ? OR lower(last_name) like ? OR lower(email) like ?", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%", "%#{params[:search].downcase}%")
+    end
+    if @users.empty?
+      @subject = Subject.find_by("lower(name) like ? ", "%#{params[:search].downcase}%")
+      unless @subject.nil?
+        @user_subject = UserSubject.find_by(subject_id: @subject.id)
+        @users = User.where(id: @user_subject.user_id)
+      end
+    end
+    if @users.empty?
       @users = User.all
     end 
   end
