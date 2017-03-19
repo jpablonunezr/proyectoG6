a200126196ad423fa0e503cf413b185957ac84ec
jpablonunezr <jpablo.nunezr@gmail.com>
2017-03-19 19:02:43 -0300

    carrierwave y aws + css

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,7 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :subject_id, :level_id])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :subject_id, :level_id, :photo])
    end
 
   # The path used after sign up.
